a147fe0215196ec85e69fc8d0be86b143d9e4723
dukegod <dukehui1991@gmail.com>
2017-09-13 00:08:43 +0800

    add menu

--- app/src/main/app.js
+++ app/src/main/app.js
@@ -7,7 +7,6 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -63,6 +62,8 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
+
+app.setName('soundCloudApp');
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
